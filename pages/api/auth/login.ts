1ea9248bff35d1d9fe3ec7d92be428f446593292
cristhianbrh <cristhianalexand@gmail.com>
2025-06-01 11:10:23 -0500

    Append modules jwt and prisma, create migration v1.0.0 and create api login and register

---
+++ pages/api/auth/login.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@/app/generated/prisma";
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user || !user.active) {
+      return res.status(401).json({ error: "Usuario no encontrado o inactivo" });
+    }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      return res.status(401).json({ error: "Credenciales inválidas" });
+    }
+    // Generar JWT
+    const token = jwt.sign(
+      { id: user.id, email: user.email, name: user.name, role: user.role },
+      process.env.JWT_SECRET || "devsecret",
+      { expiresIn: "7d" }
+    );
+    res.status(200).json({
+      message: "Login exitoso",
+      user: { id: user.id, email: user.email, name: user.name, role: user.role },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+}
---
+++ pages/api/auth/register.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@/app/generated/prisma";
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { email, password, name } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: "user", // or another appropriate default role
+      },
+    });
+    res.status(201).json({
+      message: "User created",
+      user: { id: user.id, email: user.email },
+    });
+  } catch (error) {
+    res.status(400).json({ error: "User already exists or error" });
+  }
+}
