a5871e0afab10e1978415d1aa2b7450f97e7168b
4dominic888 <4dominin888@gmail.com>
2025-06-07 21:05:17 -0500

    chore: remove a console log
    It had a lot of dirty line logs

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -24,10 +24,6 @@ export default async function handler(
         role,
       },
     });
-    // res.status(201).json({
-    //   message: "User created",
-    //   user: { id: user.id, email: user.email },
-    // });
 
     const token = jwt.sign(
       { id: user.id, email: user.email, name: user.name, role: user.role },
@@ -45,7 +41,6 @@ export default async function handler(
       token,
     });
   } catch (error) {
-    console.log(error);
     res.status(400).json({ error: "User already exists or error" });
   }
 }
